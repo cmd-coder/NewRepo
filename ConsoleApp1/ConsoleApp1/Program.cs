3d2ec4cc4841e5f078d4ce8701c42c3148645d04
CMD <gautamkailashsingh@gmail.com>
2020-09-30 19:58:24 +0530

    First commit in branch 1.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("My first repository");
+            Console.WriteLine("This is my first branch");
         }
     }
 }
